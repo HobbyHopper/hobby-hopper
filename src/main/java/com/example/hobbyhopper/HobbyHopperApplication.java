908252194f3c03ff0cccb11f10192e32e2075be6
williamxmejia <willx1995@gmail.com>
2022-05-27 09:44:46 -0500

    setting up project structure

---
+++ src/main/java/com/example/hobbyhopper/HobbyHopperApplication.java
@@ -0,0 +1,13 @@
+package com.example.hobbyhopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyHopperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyHopperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hobbyhopper/HobbyHopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hobbyhopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyHopperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
