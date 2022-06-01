4a7ead5396466f77beaea4396c34ad6b5efa8959
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-06-01 15:49:38 -0500

    Add ability to see specific event title.

--- src/seeder.sql
+++ src/seeder.sql
@@ -1,3 +1,4 @@
 use hobby_hopper;
 
-INSERT INTO events (event_name, event_description,  start_date_time) VALUES ( 'really cool event','an event that is super awesome and cool',20220512)
+INSERT INTO events (event_name, event_description,  start_date_time, category_id, is_age_restricted, is_public, rsvp_able) VALUES
+        ('really cool event','an event that is super awesome and cool',20220512, 1, false, false, true)
