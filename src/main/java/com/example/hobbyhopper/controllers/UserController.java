1e862318a9314594855244837bdc075630d84e7c
William Mejia <willx1995@gmail.com>
2022-06-01 14:36:53 -0500

    fixing static error

--- src/main/java/com/example/hobbyhopper/controllers/UserController.java
+++ src/main/java/com/example/hobbyhopper/controllers/UserController.java
@@ -5,7 +5,13 @@ import com.example.hobbyhopper.repositories.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -21,5 +27,25 @@ public class UserController {
     public String showSignupForm(Model model){
         model.addAttribute("user",new User());
         return "views/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String postUser(@ModelAttribute User user){
+
+//        if(user.getUsername().length() < 3 || user.getPassword().length() < 8 || user.getEmail().isEmpty()){
+//            validation.addError(new FieldError("user", "username", "Username Error"));
+//            validation.addError(new FieldError("user", "email", "Email Error"));
+//            validation.addError(new FieldError("user", "password", "Password Error"));
+//            if(validation.hasErrors()){
+//                model.addAttribute("errors", validation);
+//                model.addAttribute("user", user);
+//                return "views/sign-up";
+//            }
+//        }
+
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/sign-up";
     }
 }
