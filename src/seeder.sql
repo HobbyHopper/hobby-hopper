91fc75077d574c1c7729853913210d426503885e
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-06-01 17:02:13 -0500

    Add event title to head, add button styling, add rsvp hide functionality, might've missed something else but that is the bulk of it.

--- src/seeder.sql
+++ src/seeder.sql
@@ -1,4 +1,5 @@
 use hobby_hopper;
 
 INSERT INTO events (event_name, event_description,  start_date_time, category_id, is_age_restricted, is_public, rsvp_able) VALUES
-        ('really cool event','an event that is super awesome and cool',20220512, 1, false, false, true)
+        ('really cool event','an event that is super awesome and cool',20220512, 1, false, false, true),
+        ('Not RSVPable','I\'m creating this event to see if the RSVP button shows up. It shouldn\'t since the event property rsvpAble is set to false. ',20220512, 1, false, false, false)
