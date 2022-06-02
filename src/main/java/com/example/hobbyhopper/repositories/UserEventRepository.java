56f5638e160212d1b6c6a59562a2b7fd0455f342
Estebahn Molina <molina.estebahn@gmail.com>
2022-06-02 11:09:36 -0500

    add methods to create Event and UserEvent, add method to RSVP and Report. Add UserEventRepository file

---
+++ src/main/java/com/example/hobbyhopper/repositories/UserEventRepository.java
@@ -0,0 +1,8 @@
+package com.example.hobbyhopper.repositories;
+
+import com.example.hobbyhopper.models.UserEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserEventRepository extends JpaRepository<UserEvent,Long> {
+
+}
