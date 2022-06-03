a28827c5b6bcdb4734ff8664aaa304051715af28
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-06-03 14:29:06 -0500

    Add popup to map with the event address.

--- src/main/resources/static/js/mapbox.js
+++ src/main/resources/static/js/mapbox.js
@@ -1,15 +1,23 @@
+let map;
+
 function runMap(center) {
 
 	mapboxgl.accessToken = MAPBOX_API_TOKEN;
 
-	const map = new mapboxgl.Map({
+	map = new mapboxgl.Map({
 		container: 'map', // container ID
 		style: 'mapbox://styles/mapbox/streets-v11', // style URL
 		center: center, // starting position [lng, lat]
 		zoom: 8.8 // starting zoom
 	});
 
+}
 
+function addAddressMarker(center, address) {
 
+	let popup = new mapboxgl.Popup({closeOnClick: false})
+		.setLngLat(center)
+		.setHTML("<p>" + address + "</p>")
+		.addTo(map);
 
 }
