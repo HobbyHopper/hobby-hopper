fd93b38a783be961e7783c45e77979f91c4d9086
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-06-03 14:14:26 -0500

    Add map functionality to display map on the event address; still need to add a marker and details.

--- src/main/resources/static/js/mapbox.js
+++ src/main/resources/static/js/mapbox.js
@@ -1,26 +1,15 @@
-mapboxgl.accessToken = MAPBOX_API_TOKEN;
+function runMap(center) {
 
-const map = new mapboxgl.Map({
-	container: 'map', // container ID
-	style: 'mapbox://styles/mapbox/streets-v11', // style URL
-	center: centerCoords, // starting position [lng, lat]
-	zoom: 8.8 // starting zoom
-});
+	mapboxgl.accessToken = MAPBOX_API_TOKEN;
+
+	const map = new mapboxgl.Map({
+		container: 'map', // container ID
+		style: 'mapbox://styles/mapbox/streets-v11', // style URL
+		center: center, // starting position [lng, lat]
+		zoom: 8.8 // starting zoom
+	});
 
 
-// 	mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhei1zb3RvLW11bmV0IiwiYSI6ImNsMGxsZzdwczBtNzgzam8zMHZoaWNhZzIifQ.xvCWK-Woj2PD7_1cbTF70A';
-//
-// 	const map = new mapboxgl.Map({
-// 	container: 'map',
-// 	style: 'mapbox://styles/mapbox/streets-v11',
-// 	center: [-79.4512, 43.6568],
-// 	zoom: 13
-// });
-//
-// 	// Add the control to the map.
-// 	map.addControl(
-// 	new MapboxGeocoder({
-// 	accessToken: mapboxgl.accessToken,
-// 	mapboxgl: mapboxgl
-// })
-// 	);
+
+
+}
