27f9093ef72dd2aad73d3ea5ff2386f6e7f03115
William Mejia <willx1995@gmail.com>
2022-06-08 11:44:15 -0500

    feat:delete user

--- src/main/java/com/example/hobbyhopper/repositories/UserRepository.java
+++ src/main/java/com/example/hobbyhopper/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.hobbyhopper.repositories;
 
+import com.example.hobbyhopper.models.Hobby;
 import com.example.hobbyhopper.models.User;
+import com.example.hobbyhopper.models.UserEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -9,7 +11,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
-
-
-
 }
