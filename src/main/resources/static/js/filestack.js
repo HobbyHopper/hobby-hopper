fea9d790eb05e565c510e8a2e43729eb4ff9a3be
William Mejia <willx1995@gmail.com>
2022-06-10 11:39:08 -0500

    feat: reafactored the filestack api to be able to add mulitple images

--- src/main/resources/static/js/filestack.js
+++ src/main/resources/static/js/filestack.js
@@ -1,24 +1,26 @@
-const img = document.getElementsByClassName("image");
 const picker = document.getElementsByClassName("picker");
 
 
 function fileStackApi() {
-    for (let j = 0; j < img.length; j++) {
-        for (let i = 0; i < picker.length; i++) {
-            picker[i].addEventListener("click", () => {
-                const client = filestack.init(FILESTACK_API_TOKEN);
-                const options = {
-                    accept: ["image/*"],
-                    maxFiles: 5,
-                    onFileUploadFinished(file) {
-                        console.log(file)
-                        // let imageArr = [];
-                        img[j].value = file.url;
-                    }
+    for (let i = 0; i < picker.length; i++) {
+        picker[i].addEventListener("click", () => {
+            const client = filestack.init(FILESTACK_API_TOKEN);
+            const options = {
+                accept: ["image/*"],
+                maxFiles: 5,
+                onFileUploadFinished(file) {
+                    console.log(file);
+                    let element = document.createElement('input');
+                    element.setAttribute("type", "hidden");
+                    element.value = file.url;
+                    element.setAttribute("class", "image");
+                    element.setAttribute("name", "images");
+                    document.getElementById("image-url").appendChild(element);
+                    // let imageArr = [];
                 }
-                client.picker(options).open();
-            });
-        }
+            }
+            client.picker(options).open();
+        });
     }
 }
 
