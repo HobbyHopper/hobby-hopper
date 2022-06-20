e58b298f8cf1a574fb12fdf3ccd4177d14ffc7eb
William Mejia <willx1995@gmail.com>
2022-06-20 15:46:36 -0500

    changing container size on the index, adding an event listener to the delete user button

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,19 @@
+
+//Delete User
+// document.getElementById("erase-user").addEventListener("click", ()=>{
+//     let confirmAction = confirm("Are you sure you want to delete user?");
+//     if(confirmAction){
+//         window.location.replace("/user/update");
+//     }else{
+//         window.location.reload();
+//     }
+// })
+//
+//
+// //Delete event
+// let eventId = [[${event.id}]];
+// function confirmDeleteEvent() {
+//     if(confirm("Are you sure you want to delete your event?")){
+//         window.location.replace("/event/delete/" + eventId);
+//     }
+// }
